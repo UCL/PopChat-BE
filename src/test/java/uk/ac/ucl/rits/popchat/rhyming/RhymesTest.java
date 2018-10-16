88d76a2c2f97c3b8ee4a9f490ba55e90974770b9
Roma Klapaukh <r.klapaukh@ucl.ac.uk>
2018-10-16 16:16:29 +0100

    Add a test

--- src/test/java/uk/ac/ucl/rits/popchat/rhyming/RhymesTest.java
+++ src/test/java/uk/ac/ucl/rits/popchat/rhyming/RhymesTest.java
@@ -1,14 +1,32 @@
 package uk.ac.ucl.rits.popchat.rhyming;
 
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.Set;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class RhymesTest {
+
+	@Autowired
+	private MockMvc mvc;
 
 	@Test
 	public void testRhymes() {
@@ -21,5 +39,17 @@ public class RhymesTest {
 		assertTrue(matches.size() > 0);
 		assertFalse(matches.contains(word));
 		assertTrue(matches.contains("michelle"));
+	}
+
+	@Test
+	public void testApp() throws Exception {
+
+		MockHttpServletRequestBuilder query = MockMvcRequestBuilders.get("/hello");
+
+		ResultActions ra = mvc.perform(query);
+
+		assertTrue(ra.andReturn().getResponse().getStatus() == 200);
+
+		ra.andExpect(status().isOk()).andExpect(content().string(equalTo("Hello world")));
 	}
 }
