7e948e375dba04d3ca949c1fd1529095f7ed0b6c
Roma Klapaukh <r.klapaukh@ucl.ac.uk>
2018-10-26 17:47:49 +0100

    Fix the tests

--- src/test/java/uk/ac/ucl/rits/popchat/rhyming/RhymesTest.java
+++ src/test/java/uk/ac/ucl/rits/popchat/rhyming/RhymesTest.java
@@ -28,28 +28,34 @@ public class RhymesTest {
 	@Autowired
 	private MockMvc mvc;
 
+	/**
+	 * Make sure that case doesn't matter in the rhyme lookup
+	 */
 	@Test
 	public void testRhymes() {
 		Rhymes rhymes = Rhymes.getRhymes();
 
 		assertNotNull(rhymes);
 
-		String word = "Raquel";
+		String word = "RaQuEl";
 		Set<String> matches = rhymes.rhymes(word);
 		assertTrue(matches.size() > 0);
 		assertFalse(matches.contains(word));
 		assertTrue(matches.contains("michelle"));
 	}
 
+	/**
+	 *  Make sure that the rhyming end point is hooked up
+	 * @throws Exception Something bad happened
+	 */
 	@Test
 	public void testApp() throws Exception {
 
-		MockHttpServletRequestBuilder query = MockMvcRequestBuilders.get("/hello");
+		MockHttpServletRequestBuilder query = MockMvcRequestBuilders.get("/words/rhymes-with/dog");
 
 		ResultActions ra = mvc.perform(query);
 
-		assertTrue(ra.andReturn().getResponse().getStatus() == 200);
-
-		ra.andExpect(status().isOk()).andExpect(content().string(equalTo("Hello world")));
+		ra.andExpect(status().isOk()).andExpect(
+				content().json("[\"epilogue\",\"cog\",\"acog\",\"log\",\"bog\",\"haug\",\"zaugg\",\"blog\",\"fog\"]"));
 	}
 }
