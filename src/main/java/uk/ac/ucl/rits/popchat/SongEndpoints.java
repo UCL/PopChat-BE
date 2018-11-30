bd34c612c8a6c087ea44edaa1c7fcadb2777c38e
Roma Klapaukh <r.klapaukh@ucl.ac.uk>
2018-11-30 17:55:29 +0000

    Start wiring up the game playing endpoint

--- src/main/java/uk/ac/ucl/rits/popchat/SongEndpoints.java
+++ src/main/java/uk/ac/ucl/rits/popchat/SongEndpoints.java
@@ -1,14 +1,20 @@
 package uk.ac.ucl.rits.popchat;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import uk.ac.ucl.rits.popchat.messages.Game;
 import uk.ac.ucl.rits.popchat.messages.SongResponse;
+import uk.ac.ucl.rits.popchat.songs.Lyrics;
 import uk.ac.ucl.rits.popchat.songs.Song;
 import uk.ac.ucl.rits.popchat.songs.SongRepository;
 
@@ -31,6 +37,19 @@ public class SongEndpoints {
 		Pageable pageable = PageRequest.of(page, perPage);
 		Page<Song> songs = this.songRepo.findAll(pageable);
 		return songs.map(s -> new SongResponse(s));
+	}
+
+	@PostMapping("/play/{songId}")
+	public Game playGame(@PathVariable("songId") long songId) {
+		Optional<Song> songSearch = songRepo.findById(songId);
+		if (songSearch.isEmpty()) {
+			throw new IllegalArgumentException("So such song " + songId);
+		}
+		Song song = songSearch.get();
+		Lyrics lyrics = new Lyrics(song);
+		Game game = new Game(lyrics);
+
+		return game;
 	}
 
 }
---
+++ src/main/java/uk/ac/ucl/rits/popchat/messages/Game.java
@@ -0,0 +1,12 @@
+package uk.ac.ucl.rits.popchat.messages;
+
+import uk.ac.ucl.rits.popchat.songs.Lyrics;
+
+public class Game {
+
+	public Game() {
+	}
+
+	public Game(Lyrics lyrics) {
+	}
+}
